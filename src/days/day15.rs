be8367c2673a13b4c56c6335c344db957b49c894
Hugo Simonsson <simonssonhugo13@gmail.com>
2020-12-20 22:21:02 +0100

    Ran `cargo fmt` on day15.rs

--- src/days/day15.rs
+++ src/days/day15.rs
@@ -1,16 +1,17 @@
-use std::collections::HashMap;
 use crate::util::*;
-
+use std::collections::HashMap;
 
 pub fn solve() -> (Option<i64>, Option<i64>) {
     let numbers = read_file_to_string("src/days/input/15");
-    let numbers = numbers.split(',').map(|x| x.parse().unwrap()).collect::<Vec<i64>>();
+    let numbers = numbers
+        .split(',')
+        .map(|x| x.parse().unwrap())
+        .collect::<Vec<i64>>();
 
     (Some(solve1(&mut numbers.clone())), Some(solve2(numbers)))
 }
 
 fn solve1(numbers: &mut Vec<i64>) -> i64 {
-
     while numbers.len() < 2020 {
         let last_num = numbers[numbers.len() - 1];
         if !numbers.contains(&last_num) {
@@ -30,12 +31,16 @@ fn solve1(numbers: &mut Vec<i64>) -> i64 {
 }
 
 fn solve2(numbers: Vec<i64>) -> i64 {
-    let mut seen_numbers: HashMap<i64, i64> = numbers.iter().enumerate().map(|(v, &k)| (k, v as i64 + 1)).collect();
+    let mut seen_numbers: HashMap<i64, i64> = numbers
+        .iter()
+        .enumerate()
+        .map(|(v, &k)| (k, v as i64 + 1))
+        .collect();
     let mut next_number = numbers[numbers.len() - 1];
 
     for curr_idx in (numbers.len() as i64)..30000000 {
         if let Some(last_idx) = seen_numbers.get(&next_number) {
-            let diff = curr_idx- last_idx;
+            let diff = curr_idx - last_idx;
             seen_numbers.insert(next_number, curr_idx);
             next_number = diff;
         } else {
